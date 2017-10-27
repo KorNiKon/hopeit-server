6893fb53cb9e888f79dd42c7398542edffe4742c
kejn <kaen04@gmail.com>
2017-10-27 20:32:25 +0200

    Adjustment for heroku and mLab

--- src/main/java/io/kornikon/hopeit/config/MongoConfig.java
+++ src/main/java/io/kornikon/hopeit/config/MongoConfig.java
@@ -2,12 +2,16 @@ package io.kornikon.hopeit.config;
 
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import io.kornikon.hopeit.model.Child;
 import io.kornikon.hopeit.repository.KidRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+
+import java.util.Arrays;
 
 class MongoConfig extends AbstractMongoConfiguration {
 
@@ -16,6 +20,12 @@ class MongoConfig extends AbstractMongoConfiguration {
 
     @Value("${spring.data.mongodb.host}")
     private String host;
+
+    @Value("${spring.data.mongodb.username}")
+    private String username;
+
+    @Value("${spring.data.mongodb.password}")
+    private String password;
 
     @Value("${spring.data.mongodb.port}")
     private int port;
@@ -36,7 +46,9 @@ class MongoConfig extends AbstractMongoConfiguration {
 
     @Override
     public Mongo mongo() throws Exception {
-        return new MongoClient(host, port);
+        MongoCredential credentials = MongoCredential.createCredential(username, database, password.toCharArray());
+        ServerAddress serverAddress = new ServerAddress(host, port);
+        return new MongoClient(serverAddress, Arrays.asList(credentials));
     }
 
 }
